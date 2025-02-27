7cd08bed444dda6b88b0754bbaa70e3d4ce0d514
Sammy Shear <sammyshear1@gmail.com>
2025-02-27 00:37:43 -0500

    wakapi

--- config/nvim/lua/plugins/tools.lua
+++ config/nvim/lua/plugins/tools.lua
@@ -35,8 +35,6 @@ return {
       },
     },
     opts = {
-      -- if you want to open yazi instead of netrw, see below for more info
-      open_for_directories = true,
       keymaps = {
         show_help = "<f1>",
       },
