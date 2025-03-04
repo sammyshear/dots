dadd6c32f3ecadf0648e8018bcbfd18dd783ca4a
Sammy Shear <sammyshear1@gmail.com>
2025-03-04 15:35:51 -0500

    thunderbird stuff

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -1,6 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.lsp.set_log_level("off")
 vim.filetype.add({ extension = { templ = "templ" } })
 vim.filetype.add({ extension = { pgn = "pgn" } })
 vim.filetype.add({
