3c0ceae70f1b0839f6742bfb746113b4301672b9
Sammy Shear <sammyshear1@gmail.com>
2025-07-07 17:38:49 -0400

    rm tailscale

--- hosts/sammylaptoplenovo/home.nix
+++ hosts/sammylaptoplenovo/home.nix
@@ -19,7 +19,6 @@
     ../../home/programs/lazygit
     ../../home/programs/duckduckgo-colorscheme
     ../../home/programs/discord
-    ../../home/programs/tailscale
     ../../home/programs/anyrun
 
     # Scripts
