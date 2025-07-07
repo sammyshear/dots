6eb6560f0a5713c8c5618deed35c117479d48899
Sammy Shear <sammyshear1@gmail.com>
2025-07-06 22:57:01 -0400

    init nix

--- flake.nix
+++ flake.nix
@@ -1,35 +1,58 @@
 {
-  description = "Home Manager configuration of sammyshear";
+  # https://github.com/anotherhadi/nixy
+  description = ''
+    Nixy simplifies and unifies the Hyprland ecosystem with a modular, easily customizable setup.
+    It provides a structured way to manage your system configuration and dotfiles with minimal effort.
+  '';
 
   inputs = {
-    # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
+    hyprpanel.url = "github:Jas-SinghFSU/HyprPanel";
+    stylix.url = "github:danth/stylix";
+    apple-fonts.url = "github:Lyndeno/apple-fonts.nix";
+    zen-browser.url = "github:0xc000022070/zen-browser-flake";
+    nixcord.url = "github:kaylorben/nixcord";
+    sops-nix.url = "github:Mic92/sops-nix";
+    nixarr.url = "github:rasmus-kirk/nixarr";
+    anyrun.url = "github:fufexan/anyrun/launch-prefix";
+    nvf.url = "github:notashelf/nvf";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    hyprpanel.url = "github:Jas-SinghFSU/HyprPanel";
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    spicetify-nix = {
+      url = "github:Gerg-L/spicetify-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    hyprspace = {
+      url = "github:KZDKM/Hyprspace";
+      inputs.hyprland.follows = "hyprland";
+    };
+    search-nixos-api.url = "github:anotherhadi/search-nixos-api";
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, hyprpanel, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      homeConfigurations."sammyshear" =
-        home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
+  outputs = inputs @ {nixpkgs, ...}: {
+    nixosConfigurations = {
+      sammylaptoplenovo =
+        # CHANGEME: This should match the 'hostname' in your variables.nix file
+        nixpkgs.lib.nixosSystem {
           modules = [
-            ./home.nix
             {
-              nixpkgs.overlays = [ inputs.hyprpanel.overlay ];
-              _module.args = { inherit inputs; };
+              nixpkgs.overlays = [inputs.hyprpanel.overlay];
+              _module.args = {inherit inputs;};
             }
+            inputs.nixos-hardware.nixosModules.lenovo-thinkpad-t14-amd-gen3 # CHANGEME: check https://github.com/NixOS/nixos-hardware
+            inputs.home-manager.nixosModules.home-manager
+            inputs.stylix.nixosModules.stylix
+            ./hosts/sammylaptoplenovo/configuration.nix # CHANGEME: change the path to match your host folder
           ];
-          extraSpecialArgs = {
-            inherit system;
-            inherit inputs;
-          };
         };
     };
+  };
 }
---
+++ hosts/sammylaptoplenovo/variables.nix
@@ -0,0 +1,41 @@
+{
+  config,
+  lib,
+  ...
+}: {
+  imports = [
+    # Choose your theme here:
+    ../../themes/catppuccin.nix
+  ];
+
+  config.var = {
+    hostname = "sammylaptoplenovo";
+    username = "sammyshear";
+    configDirectory =
+      "/home/"
+      + config.var.username
+      + "/.config/nixos"; # The path of the nixos configuration directory
+
+    keyboardLayout = "us";
+
+    location = "New York";
+    timeZone = "America/New_York";
+    defaultLocale = "en_US.UTF-8";
+
+    git = {
+      username = "Sammy Shear";
+      email = "sammyshear1@gmail.com";
+    };
+
+    autoUpgrade = false;
+    autoGarbageCollector = true;
+  };
+
+  # Let this here
+  options = {
+    var = lib.mkOption {
+      type = lib.types.attrs;
+      default = {};
+    };
+  };
+}
